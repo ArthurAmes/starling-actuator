295317d2a61621bfe349da74b50b4af8097504b0
Arthur Ames <arthurames9@gmail.com>
2021-07-08 13:26:25 -0400

    Fix stuff

--- src/main.cpp
+++ src/main.cpp
@@ -8,54 +8,61 @@ struct Sensor {
 
 struct Result {
   uint32_t pin;
-  float volume;
+  int volume;
   int result;
 };
 
 const int sensors_count = 2;
-Sensor sensors[sensors_count] = {{10, 20}, {5, 10}};
+Sensor sensors[sensors_count] = {{17, 14}, {16, 15}};
+
+void read_bytes_until_blocking(char delimeter, char* buf, int max) {
+  while(Serial.available() <= 0) {}
+
+  Serial.readBytesUntil('\n', buf, 255);
+}
 
 void recalibrate() {
   int eeprom_idx = 0;
   char buf[255];
-  int sample_amount;
+  int sample_amount = 0;
 
   const int max_samples = (int)(((EEPROM.length() - (EEPROM.length()%sizeof(Result)))/sizeof(Result)/sensors_count));
 
   do {
     sprintf(buf, "Sample amount (max %d): ", max_samples);
     Serial.print(buf);
-    if(Serial.available() > 0) {
-      Serial.readBytesUntil('\n', buf, 255);
-      sample_amount = atoi(buf);
-      if(sample_amount > max_samples) {
-        Serial.println("ERROR: Sample amount above allowable memory limit.");
-      }
+
+    read_bytes_until_blocking('\n', buf, 255);
+    sample_amount = atoi(buf);
+    if(sample_amount > max_samples) {
+      Serial.println("ERROR: Sample amount above allowable memory limit.");
     }
   } while (sample_amount > max_samples);
 
-  for(int i = 0; i < max_samples; ++i) {
+  Serial.println(sample_amount);
+
+  for(int i = 0; i < sample_amount; ++i) {
     Serial.print("Volume (ml): ");
-    if (Serial.available() > 0) {
-      Serial.readBytesUntil('\n', buf, 255);
-      int vol = atoi(buf);
-      Serial.println(vol);
+    read_bytes_until_blocking('\n', buf, 255);
+    int vol = atoi(buf);
+    Serial.println(vol);
 
-      for(int i = 0; i < sensors_count; ++i) {
-        digitalWrite(sensors[i].ledPin, HIGH);
-        delay(10);
+    for(int i = 0; i < sensors_count; ++i) {
+      digitalWrite(sensors[i].ledPin, HIGH);
+      delay(10);
 
-        int result = analogRead(sensors[i].resistorPin);
-        digitalWrite(sensors[i].ledPin, LOW);
-        delay(10);
+      int result = analogRead(sensors[i].resistorPin);
+      digitalWrite(sensors[i].ledPin, LOW);
+      delay(10);
 
-        sprintf(buf, "Volume: %d | Sensor: %d | Result: %d\n", vol, i, result);
-        Serial.print(buf);
+      Result res{sensors[i].resistorPin, vol, result};
+      EEPROM.put(eeprom_idx, res);
+      EEPROM.commit();
 
-        Result res{sensors[i].resistorPin, vol, result};
-        EEPROM.put(eeprom_idx, &res);
-        eeprom_idx += sizeof(res);
-      }
+      sprintf(buf, "Volume: %d | Sensor: %d | Result: %d\n", res.volume, res.pin, res.result);
+      Serial.print(buf);
+
+      eeprom_idx += sizeof(Result);
     }
   }
 }
@@ -64,8 +71,12 @@ void print_results() {
   char buf[255];
 
   for(int i = 0; i < EEPROM.length(); i += sizeof(Result)) {
-    Result res{};
+    Result res;
     EEPROM.get(i, res);
+
+    if(res.pin == -1) {
+      break;
+    }
 
     sprintf(buf, "Volume: %d | Sensor: %d | Result: %d\n", res.volume, res.pin, res.result);
     Serial.print(buf);
@@ -74,13 +85,14 @@ void print_results() {
 
 void setup() {
   Serial.begin(9600);
+  EEPROM.begin(1024);
   for(Sensor sensor : sensors) {
     pinMode(sensor.ledPin, OUTPUT);
     pinMode(sensor.resistorPin, INPUT);
   }
 
+  Serial.println("Recalibrate? Y/n");
   while(true) {
-    Serial.println("Recalibrate? Y/n");
     if (Serial.available() > 0) {
       char rx_byte = Serial.read();
       if(rx_byte == 'Y' || rx_byte == 'y') {
@@ -91,6 +103,8 @@ void setup() {
       }
     }
   }
+
+  Serial.println("Printing Results: \n");
 
   print_results();
 }
